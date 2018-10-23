c9dd4c678403ed020a8829e202add5aa6f800a06
DESKTOP-2JKUURD\bruno <bruno.xavier.moura@gmail.com>
2018-10-23 20:57:18 -0300

    adding AssemblyInfo.

---
+++ src/CSharpProperties.DependencyInjection/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("MosaicoSolutions")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("2018")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Dependency Injection for CSharpProperties")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("CSharpProperties.DependencyInjection")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CSharpProperties.DependencyInjection")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
